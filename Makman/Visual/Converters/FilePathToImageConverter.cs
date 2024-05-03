c98aa329ac5b2381c951e80210228a1518c3decd
MaKeMaK <73843102+MaKeMaK-lee@users.noreply.github.com>
2024-05-03 10:27:04 +0300

    Added normal FilePathToImageConverter, switched to him some images in CollectionView and UnitsManagerView

---
+++ Makman/Visual/Converters/FilePathToImageConverter.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.WindowsAPICodePack.Shell;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Makman.Visual.Converters
+{
+    [ValueConversion(typeof(string), typeof(object))]
+    public class FilePathToImageConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+            string path = (string)value;
+            if (string.IsNullOrEmpty(path))
+                return null;
+
+
+            var shellObject = ShellObject.FromParsingName(path);
+            string contentType = (string)shellObject.Properties.GetProperty("System.ContentType").ValueAsObject;
+
+            if (contentType.StartsWith("image"))
+            {
+                try
+                {
+                    return new BitmapImage(new Uri(path));
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+            return shellObject.Thumbnail.ExtraLargeBitmapSource;
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
